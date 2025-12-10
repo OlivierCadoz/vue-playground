c0d1cb8debf53fb5a98ac70a6951a7ac1ad68595
OlivierCadoz <cadoz.olivier@gmail.com>
2025-12-10 10:54:48 +0100

    style(config): add scss preprocessor

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,23 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { fileURLToPath, URL } from 'node:url'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  css: {
+    preprocessorOptions: {
+      // scss: {
+      //   additionalData: `
+      //     @import "@/scss/_variables.scss";
+      //     @import "@/scss/_mixins.scss";
+      //   `
+      // }
+    }
+  },
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
 })
