b8196ea788445d96ce2a7dbdaee666149225d2e4
OlivierCadoz <cadoz.olivier@gmail.com>
2025-12-10 15:15:01 +0100

    feat(component): add modal for form

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,22 @@
 <script setup>
-import FormVP from './components/organisms/FormVP.vue';
 import TaskList from './components/organisms/TaskList.vue';
+import TaskFormModal from './components/organisms/TaskFormModal.vue';
 import ButtonVP from './components/atoms/ButtonVP.vue';
 import { useTask } from './composables/useTask';
 
 const { tasks, showTaskForm, addTask, toggleTaskForm } = useTask();
 
+const submitForm = (task) => {
+  addTask(task);
+  toggleTaskForm();
+};
 </script>
 
 <template>
   <main>
     <ButtonVP @click="toggleTaskForm">Ajouter +</ButtonVP>
-    <FormVP v-if="showTaskForm" @submitForm="addTask" />
     <TaskList :tasks="tasks" />
+
+    <TaskFormModal v-if="showTaskForm" @submitForm="submitForm" />
   </main>
 </template>
