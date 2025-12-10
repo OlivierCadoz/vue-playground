dedf7fe44b67efde8ecb8a032a4dee8a43fd864f
OlivierCadoz <cadoz.olivier@gmail.com>
2025-12-10 14:58:11 +0100

    feat(component): add modal for form

---
+++ src/components/organisms/TaskFormModal.vue
@@ -0,0 +1,30 @@
+<script setup>
+import { ref, computed } from 'vue'
+
+import ModalVP from '../atoms/ModalVP.vue';
+import InputLabeledVP from '../molecules/InputLabeledVP.vue';
+import ButtonVP from '../atoms/ButtonVP.vue';
+
+const emit = defineEmits(['submitForm'])
+
+const modelValue = ref('')
+
+const isEmpty = computed(() => !modelValue.value.trim())
+
+const submitForm = () => {
+  const value = modelValue.value.trim()
+  if (!value) return
+  emit('submitForm', value)
+  modelValue.value = ''
+}
+
+</script>
+
+<template>
+  <ModalVP>
+    <form @submit.prevent="submitForm">
+      <InputLabeledVP v-model="modelValue" required>Add task :</InputLabeledVP>
+      <ButtonVP type="submit" :disabled="isEmpty">Add</ButtonVP>
+    </form>
+  </ModalVP>
+</template>
